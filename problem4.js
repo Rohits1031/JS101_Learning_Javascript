3f3a18862c7f1282326b491e8902273ca23c8d67
Rohit <rsahu9560@gmail.com>
2023-04-28 02:12:51 +0530

    Report card

---
+++ problem4.js
@@ -0,0 +1,25 @@
+// Report Card
+
+let name = "Rapchik";
+let school = "KVS";
+let grade = "A++"
+let section = "B"
+let rollNo = "69";
+let sub1 = "Maths";
+let sub2 = "Science";
+let sub3 = "English";
+let marks1 = 90;
+let marks2 = 85;
+let marks3 = 92;
+
+
+console.log("Report Card");
+console.log("------------");
+console.log("Name: " + name);
+console.log("School: " + school);
+console.log("Geade: " + grade);
+console.log("Section: " + section);
+console.log("Roll No.: " + rollNo);
+console.log("Subject 1 (" + sub1 + ") Marks: " + marks1);
+console.log("Subject 2 (" + sub2 + ") Marks: " + marks2);
+console.log("Subject 3 (" + sub3 + ") Marks: " + marks3);
